98d9937d1b61d50e9310aded7a6cad97196ed91a
Jihee hwang <hwangjihee94@gmail.com>
2021-11-11 18:14:12 +0900

    no message

--- practice_Measure/practice_Measure/ViewController.swift
+++ practice_Measure/practice_Measure/ViewController.swift
@@ -1,51 +1,59 @@
 //
-//  ViewController.swift
-//  practice_Measure
-//
-//  Created by Jihee hwang on 2021/11/10.
-//
-
+ //  ViewController.swift
+ //  practice_Measure
+ //
+ //  Created by Jihee hwang on 2021/11/10.
+ //
+/*
 import UIKit
 
-/*
-// 결과를 한 번 더 언래핑해야 Int형이 된다.
-// 정수를 한 개 입력받을 땐 이 한 줄을 이용하면 된다.
-let input = Int(readLine()!)!
-*/
-
-/*
-let inputNum = readLine()
-if var fromInch = Int(inputNum ?? 0) {
-    print("\(inputNum) * 3")
-}ㅊ$
-*/
-
 class ViewController: UIViewController {
+
 
     @IBOutlet weak var inputNum: UITextField!
     @IBOutlet weak var convertValue: UILabel!
     
-    @IBAction func pressedButton(_ sender: UIButton) {
-        
-    }
+    func numValue(inputNum: Float) -> Any {
+        return inputNum * 2.54
     
-    /*    // override는 상속되는 슈퍼클래스의 메서드나 프로퍼티를 내가 마음대로 바꿀 수 있게 함
-    // viewDidLoad() 이 함수가 호출되어졌을때 딱 한 번만 호출되어지는 함수
+    @IBAction func convertNum(_ sender: UIButton) {
+        convertValue.text =
+    }
+        
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        configureUI()
     }
     
-    // MARK - Configure
-    func configureUI() {
-        view.backgroundColor = .black
-    } */
+}
+*/
 
+import UIKit
 
+class ViewController: UIViewController {
     
+    struct convertNumber {
+        var toCM = Float()
+        init(from inch: Float) {
+            toCM = inch * 2.54
+        }
+        func printValue() -> Float {
+            return toCM
+        }
+    }
     
+    @IBOutlet weak var inputNum: UITextField!
+    @IBOutlet weak var convertValue: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func convertBtn(_ sender: UIButton) {
+        var num = Float(inputNum.text!)
+       
+    }
+    
+
 }
 
 
