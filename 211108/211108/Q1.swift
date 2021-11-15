77fc3ffb71a45ecacbc751d940b533f2313ec852
Jihee hwang <hwangjihee94@gmail.com>
2021-11-15 20:07:03 +0900

    no message

--- 211108/211108/Q1.swift
+++ 211108/211108/Q1.swift
@@ -38,32 +38,60 @@ var myArray = printArray */
 
 // 시도 3.
   
-struct ArrayPractice {
-    var arrTwo: [[Int]] = [[1],
-                        [6, 7],
-                        [11, 12, 13],
-                        [16, 17, 18, 19],
-                        [21, 22, 23, 24, 25]]
-
-    func printArray(array: Any) {
-        for arr in arrTwo {
-            print(arr)
-        }
-    }
-}
+//struct ArrayPractice {
+//    var arrTwo: [[Int]] = [[1],
+//                        [6, 7],
+//                        [11, 12, 13],
+//                        [16, 17, 18, 19],
+//                        [21, 22, 23, 24, 25]]
+//
+//    func printArray(array: Any) {
+//        for arr in arrTwo {
+//            print(arr)
+//        }
+//    }
+//}
 
 
 // Eddy가 말한 것 처럼, 이중 for문으로 다시 만들어보자!
 // 시도 4.
 
-func printNumbers(lines: Int) {
-    var num = Int()
-    for i in 1...lines { // 행을 만듬
-        for _ in 1...i { // 열을 만듬
-            num += 1     // 빈 변수에 값을 1씩 더해 넣음
-            print(num, terminator: "")
+
+//func printNumbers(lines: Int) {
+//    var num = Int()
+//    for i in 1...lines { // 행을 만듬
+//        for _ in 1...i { // 열을 만듬
+//            num += 1     // 빈 변수에 값을 1씩 더해 넣음
+//            print(num, terminator: "")
+//        }
+//        print()
+//    }
+//}
+//printNumbers(lines: 5)
+//
+
+struct ArrayPractice {
+
+    func fillArray() -> [[Int]] {
+        let lines = 5 // 숫자가 달라지면, 배열의 크기도 달라지도록
+        var resultArr = [[Int]]() // 결과 값을 담을 2차원 배열 선언
+        var num = Int() // 값이 들어 갈 프로퍼티 선언
+        
+        for row in 1...lines {
+            var tempArray = [Int]() // 열의 값을 저장 할 배열 선언
+            for col in 1...row {
+                num += 1 // 하나씩 더하겠다.
+                tempArray.append(num) // 열의 값이 들어갈 변수에 넣어주겠다.
+            }
+        resultArr.append(tempArray)
         }
-        print()
+        num += 5
     }
+    
+    func printArray(array: [[Int]]) {
+        for innerArray in array {
+                 print(innerArray)
+        }
+    }
+    return resultArr
 }
-printNumbers(lines: 5)
